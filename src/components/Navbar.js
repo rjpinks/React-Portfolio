31a98c72a16a072f8944eb7d3f727a9f44922e49
Ryan <pinkstonr54@gmail.com>
2023-06-19 11:58:24 -0500

    initial set-up and added Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <p>About Me</p>
+            <p>Portfolio</p>
+            <p>Contact Info</p>
+            <p>Resume</p>
+        </nav>
+    )
+}
+
+export default Navbar
