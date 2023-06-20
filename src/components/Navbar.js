abd2e1d5738b37bbe5a275d042091a51e1634f4e
Ryan <pinkstonr54@gmail.com>
2023-06-20 07:23:27 -0500

    fixed errors in console

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Navbar = () => {
     return (
         <header>
-            <a href="#about" class="section-link">About Me</a>
-            <a href="#portfolio" class="section-link">Portfolio</a>
-            <a href="#contact" class="section-link">Contact Info</a>
-            <a href="#resume" class="section-link">Resumé</a>
+            <a href="#about" className="section-link">About Me</a>
+            <a href="#portfolio" className="section-link">Portfolio</a>
+            <a href="#contact" className="section-link">Contact Info</a>
+            <a href="#resume" className="section-link">Resumé</a>
         </header>
     )
 }
